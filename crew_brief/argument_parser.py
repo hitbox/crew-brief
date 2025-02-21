eb4e56154be906c3d708eab941940add938ebba4
Carl Harris <hitbox@ryzen.com>
2025-02-21 05:41:33 -0500

    More docs and formatting.

--- crew_brief/argument_parser.py
+++ crew_brief/argument_parser.py
@@ -24,12 +24,69 @@ import openpyxl
 
 from . import commands
 
+def add_drill_keys_argument(parser):
+    parser.add_argument(
+        'keys',
+        nargs = '+',
+        help =
+            'Sequence of keys into pickle database. Probably want "member_data'
+            ' userEvents eventDetails"',
+    )
+    parser.add_argument(
+        '--ignore-missing',
+        action = 'store_true',
+        help = 'Ignore missing keys.',
+    )
+
 def add_config_option(parser):
     parser.add_argument(
         '--config',
         help =
             'Path to the configuration file. If not provided, the'
             ' CREW_BRIEF_CONFIG environment variable will be used.',
+    )
+
+def add_do_schema_option(parser):
+    parser.add_argument(
+        '--schema',
+        dest = 'schema',
+        action = 'store_true',
+        help = 'Convert string data from database.',
+    )
+    parser.add_argument(
+        '--no-schema',
+        dest = 'schema',
+        action = 'store_false',
+        help = 'Do not convert string data from database.',
+    )
+    parser.set_defaults(
+        schema = True,
+    )
+
+def add_check_database_parser(subparsers):
+    """
+    Add check database sub-command parser.
+    """
+    check_database_parser = subparsers.add_parser(
+        'check',
+        help = 'Check assumptions against pickle database.',
+        description =
+            'Traverse the pickle database and confirm assumptions. That'
+            ' eventDetails is a dict, for instance.',
+    )
+    check_database_parser.set_defaults(func=commands.check_database)
+    add_config_option(check_database_parser)
+
+def add_discover_structure_parser(subparsers):
+    discover_structure_parser = subparsers.add_parser(
+        'discover_structure',
+        help = commands.discover_structure.__doc__,
+    )
+    add_drill_keys_argument(discover_structure_parser)
+    add_do_schema_option(discover_structure_parser)
+    add_config_option(discover_structure_parser)
+    discover_structure_parser.set_defaults(
+        func = commands.discover_structure,
     )
 
 def add_init_database_parser(subparsers):
@@ -39,9 +96,32 @@ def add_init_database_parser(subparsers):
     init_database_parser = subparsers.add_parser(
         'init',
         help = 'Initialize database.',
+        description =
+            'Initialize a database of JSON data from the ZIP files for'
+            ' faster testing.',
     )
     init_database_parser.set_defaults(func=commands.init_database)
     add_config_option(init_database_parser)
+
+def add_look_parser(subparsers):
+    """
+    Add parser for look command.
+    """
+    look_parser = subparsers.add_parser(
+        'look',
+        help = commands.look.__doc__,
+    )
+    look_parser.add_argument(
+        '--unique',
+        action = 'store_true',
+        help = 'Print only unique values.',
+    )
+    add_drill_keys_argument(look_parser)
+    add_do_schema_option(look_parser)
+    add_config_option(look_parser)
+    look_parser.set_defaults(
+        func = commands.look,
+    )
 
 def add_sample_output_parser(subparsers):
     """
@@ -65,80 +145,15 @@ def add_sample_output_parser(subparsers):
         default = False,
         help = 'Use Excel to autofit workbook.',
     )
+    sample_output_parser.add_argument(
+        '--flattened',
+        action = 'store_true',
+        help =
+            'Use single rows to write eventDetails to.'
+            ' Otherwise create sub-tables.',
+    )
     sample_output_parser.set_defaults(func=commands.sample_output)
     add_config_option(sample_output_parser)
-
-def add_check_database_parser(subparsers):
-    """
-    Add check database sub-command parser.
-    """
-    check_database_parser = subparsers.add_parser(
-        'check',
-        help = 'Check pickle database against assumptions.',
-    )
-    check_database_parser.set_defaults(func=commands.check_database)
-    add_config_option(check_database_parser)
-
-def add_drill_keys_argument(parser):
-    parser.add_argument(
-        'keys',
-        nargs = '+',
-        help = 'Sequence of keys into pickle database.',
-    )
-    parser.add_argument(
-        '--ignore-missing',
-        action = 'store_true',
-        help = 'Ignore missing keys.',
-    )
-
-def add_do_schema_option(parser):
-    parser.add_argument(
-        '--schema',
-        dest = 'schema',
-        action = 'store_true',
-        help = 'Convert string data from database.',
-    )
-    parser.add_argument(
-        '--no-schema',
-        dest = 'schema',
-        action = 'store_false',
-        help = 'Do not convert string data from database.',
-    )
-    parser.set_defaults(
-        schema = True,
-    )
-
-def add_look_parser(subparsers):
-    """
-    Add parser for look command.
-    """
-    look_parser = subparsers.add_parser(
-        'look',
-        help = commands.look.__doc__,
-    )
-    look_parser.add_argument(
-        '--unique',
-        action = 'store_true',
-        help = 'Print only unique values.',
-    )
-    add_drill_keys_argument(look_parser)
-    add_do_schema_option(look_parser)
-    add_config_option(look_parser)
-    look_parser.set_defaults(
-        func = commands.look,
-    )
-
-def add_discover_structure_parser(subparsers):
-    discover_structure_parser = subparsers.add_parser(
-        'discover_structure',
-        help = commands.discover_structure.__doc__,
-    )
-    add_drill_keys_argument(discover_structure_parser)
-    add_do_schema_option(discover_structure_parser)
-    add_config_option(discover_structure_parser)
-    discover_structure_parser.set_defaults(
-        func = commands.discover_structure,
-    )
 
 def add_unique_event_details(subparsers):
     parser = subparsers.add_parser(
@@ -153,6 +168,7 @@ def add_unique_event_details(subparsers):
 
 def argument_parser():
     parser = argparse.ArgumentParser(
+        prog = 'crew_brief',
         description =
             'Rewrite crew brief JSON files as more user friendly Excel files.',
     )
@@ -160,11 +176,11 @@ def argument_parser():
 
     subparsers = parser.add_subparsers()
 
-    add_init_database_parser(subparsers)
-    add_sample_output_parser(subparsers)
     add_check_database_parser(subparsers)
-    add_look_parser(subparsers)
     add_discover_structure_parser(subparsers)
+    add_init_database_parser(subparsers)
+    add_look_parser(subparsers)
+    add_sample_output_parser(subparsers)
     add_unique_event_details(subparsers)
 
     # Default to normal_run withoutput command given.
